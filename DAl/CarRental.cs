f2bdd3359c2aeb229de81260323324d3c7eecd72
brinda <brindashree1996@gmail.com>
2023-11-16 11:14:50 +0530

    Add project files.

---
+++ DAl/CarRental.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+public partial class CarRentalContext : DbContext
+{
+    public CarRentalContext(DbContextOptions<CarRentalContext> options)
+        : base("Name:ConnectionString")
+    {
+
+    }
+
+    // DbSet for each entity
+    public virtual DbSet<Admin> Admins { get; set; }
+    public virtual DbSet<Car> Cars { get; set; }
+    public virtual DbSet<Cost> Costs { get; set; }
+    public virtual DbSet<Customer> Customers { get; set; }
+    public virtual DbSet<Rental> Rentals { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+
+    }
+}
